ff076ed6752abc81c8c43803d0e88107749e9702
rramirezg18 <rramirezg18@miumg.edu.gt>
2024-03-20 21:13:57 -0600

    first commit

---
+++ src/main/java/com/mycompany/tarea2programacion3/MetodosDeOrdenamiento.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tarea2programacion3;
+
+/**
+ *
+ * @author ianto
+ */
+public class MetodosDeOrdenamiento {
+    
+    public static void selectionSort(char[] arreglo){
+        for (int i = 0; i < arreglo.length -1; i++){
+            int minIdx = i;
+            for(int j = i + 1; j < arreglo.length; j++){
+                if(arreglo[j] < arreglo[minIdx]){
+                    minIdx = j;
+                }  
+            }
+            char temp = arreglo[minIdx];
+            arreglo[minIdx] = arreglo[i];
+            arreglo[i] = temp;
+        }
+    }
+    
+    public static void bubbleSort(char[] arreglo){
+        for(int i = 0; i < arreglo.length - 1; i++){
+            for(int j = 0; j < arreglo.length - i - 1; j++){
+                if(arreglo[j] > arreglo[j+1]){
+                    char temp = arreglo[j];
+                    arreglo[j] = arreglo[j+1];
+                    arreglo[j+1] = temp;
+                }
+            }
+        }
+    }
+    
+    public static void insertionSort(char[] arreglo){
+        
+    }
+}
---
+++ src/main/java/com/mycompany/tarea2programacion3/Tarea2Programacion3.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tarea2programacion3;
+
+/**
+ *
+ * @author ianto
+ */
+import java.util.*;
+public class Tarea2Programacion3 {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        String cadena = "";
+        System.out.println("Ingresa una cadena de texto");
+        cadena = entrada.nextLine();
+        char[] arreglo = cadena.toCharArray();
+        int opcion = 0;
+        int salir = 0;
+        while(salir != 1){
+            Menu();
+            opcion = entrada.nextInt();
+            switch(opcion){
+                case 1:
+                    
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                case 8:
+                    break;
+                case 9:
+                    break;
+                case 10:
+                    break;
+            }
+        }
+    }
+    
+    public static void Menu(){
+        System.out.println("/n/t METODOS DE ORDENAMIENTO");
+        System.out.println("1. Selection Sort");
+        System.out.println("2. Bubble Sort");
+        System.out.println("3. Insertion Sort");
+        System.out.println("4. Merge Sort");
+        System.out.println("5. Quick Sort");
+        System.out.println("6. Heap Sort");
+        System.out.println("7. Counting Sort");
+        System.out.println("8. Radix Sort");
+        System.out.println("9. Bucket Sort");
+        System.out.println("10. Salir ");
+        
+    }
+}
